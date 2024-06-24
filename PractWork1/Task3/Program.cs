c93b4f41f6b074e83b7dea5e310e8f8a1416eb8e
Etiloviy-Donny <egorpermogorskiy@mail.ru>
2024-06-25 00:32:34 +0300

    Add files via upload

---
+++ PractWork1/Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿//Task3
+Console.WriteLine("Введите имя файла:");
+string filename = Console.ReadLine();
+Console.WriteLine("Введите текст для поиска:");
+string text = Console.ReadLine();
+
+if (File.Exists(filename))
+{
+    string[] lines = File.ReadAllLines(filename);
+    for (int i=0; i< lines.Length; i++)
+    {
+        if (lines[i].Contains(text))
+            Console.WriteLine($"Текст содержится в строке {i+1}");
+    }
+}
+else
+    Console.WriteLine("Такого файла не существует");
