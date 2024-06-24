db2e1d97a048f2ff53ac21fe3c2ee5f85a3ddb9c
Etiloviy-Donny <egorpermogorskiy@mail.ru>
2024-06-25 00:19:37 +0300

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+        void PrintInfo()
+        {
+        
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="a"></param>
+        /// <param name="b"></param>
+        /// <returns></returns>
+        /// <exception cref="OverflowException">сумма больше допустимого значения</exception>>
+        double GetSum (double a, double b) 
+        {
+        return a + b;
+        }
+    }
+}
